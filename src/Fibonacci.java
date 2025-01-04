83b7a1d0bf8694d022a104cffd6e32d14c1bea99
ratneeshh <ratnesha.ug23.cs@nitp.ac.in>
2025-01-04 19:44:43 +0530

    fibo dp

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,16 +1,35 @@
+import java.util.HashMap;
+
 public class Fibonacci {
+//    public static void main(String[] args) {
+//        int a = fibo(8);
+//        System.out.println(a);
+//    }
+//    static int fibo(int n) {
+//        if(n==0 || n==1)
+//            return n;
+//
+//            return fibo(n-1)+fibo(n-2);
+//    }
+
     public static void main(String[] args) {
-        int a = fibo(8);
+
+        int a = fibo(8 , new HashMap<>());
         System.out.println(a);
     }
-    static int fibo(int n) {
-        if(n==0)
-            return 0;
 
-        if(n==1)
-            return 1;
+    static int fibo(int n ,HashMap<Integer , Integer> memo ) {
 
-        else
-            return fibo(n-1)+fibo(n-2);
+        if(n==0 || n==1)
+            return n;
+
+        if(memo.containsKey(n))
+            return memo.get(n);
+
+        int result = fibo(n-1 , memo) + fibo(n-2 , memo);
+        memo.put(n , result);
+        return result;
+
+
     }
 }
