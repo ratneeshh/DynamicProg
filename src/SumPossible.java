b0910d100d3e73fa432c2b2b9b17e1db46378497
ratneeshh <ratnesha.ug23.cs@nitp.ac.in>
2025-01-05 07:59:31 +0530

    sum possible dp

--- src/SumPossible.java
+++ src/SumPossible.java
@@ -1,13 +1,59 @@
-
+import java.util.HashMap;
 
 public class SumPossible {
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        int[] arr = {1,2,3};
+//        System.out.println(testSum(5 , arr));
+//    }
+//    static boolean testSum(int n, int[] arr) {
+//
+//        if(n == 0)
+//            return true;
+//
+//        if (n < 0) {
+//            return false;
+//        }
+//
+//        for (int i = 0; i < arr.length ; i++) {
+//
+//            if(testSum(n-arr[i] , arr)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int[] arr = {1,2,3};
+//        System.out.println(testSum(5 , arr));
+//    }
+//    static boolean testSum(int n, int[] arr) {
+//
+//        if(n == 0)
+//            return true;
+//
+//        if (n < 0) {
+//            return false;
+//        }
+//
+//        for (int i = 0; i < arr.length ; i++) {
+//
+//            if(testSum(n-arr[i] , arr)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//
+public static void main(String[] args) {
 
-        int[] arr = {1,2,3};
-        System.out.println(testSum(5 , arr));
-    }
-    static boolean testSum(int n, int[] arr) {
+    int[] arr = {1,2,3};
+    System.out.println(testSum(5 , arr  , new HashMap<>()));
+}
+    static boolean testSum(int n, int[] arr, HashMap<Integer , Boolean> memo) {
 
         if(n == 0)
             return true;
@@ -16,14 +62,19 @@ public class SumPossible {
             return false;
         }
 
+        if(memo.containsKey(n))
+            return memo.get(n);
+
         for (int i = 0; i < arr.length ; i++) {
 
-            if(testSum(n-arr[i] , arr)) {
+            if(testSum(n-arr[i] , arr , memo)) {
                 return true;
             }
         }
+
+        memo.put(n , false);
         return false;
 
-
     }
+
 }
