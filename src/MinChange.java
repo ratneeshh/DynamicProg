1841d76ec01d77d90adf55f04485262d4edc5573
ratneeshh <ratnesha.ug23.cs@nitp.ac.in>
2025-01-05 09:20:28 +0530

    min change dp

--- src/MinChange.java
+++ src/MinChange.java
@@ -1,14 +1,52 @@
+import java.util.HashMap;
+
 public class MinChange {
+
+//    public static void main(String[] args) {
+//
+//        int coins[] = {1,2,3};
+//        int a = minC(5, coins);
+//        System.out.println(a);
+//
+//    }
+//
+//    static int minC (int amount , int[] coins) {
+//
+//        if(amount == 0)
+//            return 0;
+//
+//        if(amount < 0)
+//            return -1;
+//
+//        int minCoins = -1;
+//
+//        for(int num : coins) {
+//
+//            int subAmt = amount - num;
+//
+//            int subCoins = minC(subAmt , coins);
+//
+//            if(subCoins != -1) {
+//                int numCoins = subCoins+1;
+//
+//                if((numCoins < minCoins) || (minCoins == -1)) {
+//                    minCoins = numCoins;
+//                }
+//            }
+//        }
+//        return minCoins;
+//
+//    }
 
     public static void main(String[] args) {
 
         int coins[] = {1,2,3};
-        int a = minC(5, coins);
+        int a = minC(5, coins , new HashMap<>());
         System.out.println(a);
 
     }
 
-    static int minC (int amount , int[] coins) {
+    static int minC (int amount , int[] coins , HashMap<Integer,Integer> memo) {
 
         if(amount == 0)
             return 0;
@@ -16,13 +54,16 @@ public class MinChange {
         if(amount < 0)
             return -1;
 
+        if(memo.containsKey(amount))
+            return memo.get(amount);
+
         int minCoins = -1;
 
         for(int num : coins) {
 
             int subAmt = amount - num;
 
-            int subCoins = minC(subAmt , coins);
+            int subCoins = minC(subAmt , coins , memo);
 
             if(subCoins != -1) {
                 int numCoins = subCoins+1;
@@ -32,6 +73,7 @@ public class MinChange {
                 }
             }
         }
+        memo.put(amount,minCoins);
         return minCoins;
 
     }
